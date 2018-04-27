f6481b9c5320eb886e58350fe220f127772d72a8
rizki <rizkyalfianto1945@gmail.com>
2018-04-27 16:23:20 +0700

    update

--- app.py
+++ app.py
@@ -40,8 +40,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+        'test')
 
 
 if __name__ == "__main__":
