d8e5c40f2fc47ef2be97a5e96586d3d42f906a21
rizki <rizkyalfianto1945@gmail.com>
2018-04-27 16:16:29 +0700

    update1

--- app.py
+++ app.py
@@ -29,19 +29,24 @@ def callback():
     app.logger.info("Request body: " + body)
 
     # handle webhook body
-    try:
-        handler.handle(body, signature)
+        try:
+        events = parser.parse(body, signature)
     except InvalidSignatureError:
         abort(400)
 
+    # if event is MessageEvent and message is TextMessage, then echo text
+    for event in events:
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessage):
+            continue
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
+
     return 'OK'
-
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
