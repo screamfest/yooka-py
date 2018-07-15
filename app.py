df727e14c0edc8dde839ed7f54d40dcabee7cc3e
Kukuh Pradityo <kukuhpradityo@gmail.com>
2018-07-15 18:06:15 +0700

    tambah button

--- app.py
+++ app.py
@@ -52,10 +52,42 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=a))
-    elif(b=="assalamualaikum"):
+    elif(b=="info pmb"):
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text="Waalaikumsalam"))
+            {
+  "type": "template",
+  "altText": "this is a buttons template",
+  "template": {
+    "type": "buttons",
+    "actions": [
+      {
+        "type": "message",
+        "label": "Pendaftaran PMB",
+        "text": "Informasi pendaftaran PMB"
+      },
+      {
+        "type": "uri",
+        "label": "Web Unsada",
+        "uri": "https://unsada.ac.id"
+      },
+      {
+        "type": "postback",
+        "label": "Biaya Kuliah",
+        "text": "Biaya Kuliah di Unsada",
+        "data": "data_biaya_kuliah"
+      },
+      {
+        "type": "message",
+        "label": "Daftar Jurusan",
+        "text": "Data Fakultas Unsada"
+      }
+    ],
+    "thumbnailImageUrl": "SPECIFY_YOUR_IMAGE_URL",
+    "title": "Info PMB",
+    "text": "Informasi Penerimaan Mahasiswa Baru"
+  }
+})
     elif(b=="selamat pagi"):
         line_bot_api.reply_message(
             event.reply_token,
