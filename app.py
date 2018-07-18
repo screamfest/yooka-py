e2d5218a0780346d6870414131458f6927bb3c9a
Kukuh Pradityo <kukuhpradityo@gmail.com>
2018-07-19 01:10:26 +0700

    adding on new repo

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import os
 import psycopg2
+<<<<<<< HEAD
+=======
+import botbuttons
+>>>>>>> 3b75af30f0813bbb8b993aacbc70f5eb8148add6
 
 DATABASE_URL = os.environ['DATABASE_URL']
 
@@ -13,8 +17,116 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
+<<<<<<< HEAD
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
+=======
+from linebot.models.actions import (
+    Action,
+    PostbackAction,
+    MessageAction,
+    URIAction,
+    DatetimePickerAction,
+    Action as TemplateAction,  # backward compatibility
+    PostbackAction as PostbackTemplateAction,  # backward compatibility
+    MessageAction as MessageTemplateAction,  # backward compatibility
+    URIAction as URITemplateAction,  # backward compatibility
+    DatetimePickerAction as DatetimePickerTemplateAction,  # backward compatibility
+)
+from linebot.models.base import (
+    Base,
+)
+from linebot.models.error import (
+    Error,
+    ErrorDetail,
+)
+from linebot.models.events import (
+    Event,
+    MessageEvent,
+    FollowEvent,
+    UnfollowEvent,
+    JoinEvent,
+    LeaveEvent,
+    PostbackEvent,
+    AccountLinkEvent,
+    BeaconEvent,
+    Postback,
+    Beacon,
+    Link,
+)
+from linebot.models.messages import (
+    Message,
+    TextMessage,
+    ImageMessage,
+    VideoMessage,
+    AudioMessage,
+    LocationMessage,
+    StickerMessage,
+    FileMessage,
+)
+from linebot.models.imagemap import (
+    ImagemapSendMessage,
+    BaseSize,
+    ImagemapAction,
+    URIImagemapAction,
+    MessageImagemapAction,
+    ImagemapArea,
+)
+from linebot.models.responses import (
+    Profile,
+    MemberIds,
+    Content,
+    RichMenuResponse,
+    Content as MessageContent,
+)
+from linebot.models.flex_message import (
+    FlexSendMessage,
+    FlexContainer,
+    BubbleContainer,
+    BubbleStyle,
+    BlockStyle,
+    CarouselContainer,
+    FlexComponent,
+    BoxComponent,
+    ButtonComponent,
+    FillerComponent,
+    IconComponent,
+    ImageComponent,
+    SeparatorComponent,
+    SpacerComponent,
+    TextComponent
+)
+from linebot.models.rich_menu import (
+    RichMenu,
+    RichMenuSize,
+    RichMenuArea,
+    RichMenuBounds,
+)
+from linebot.models.send_messages import (
+    SendMessage,
+    TextSendMessage,
+    ImageSendMessage,
+    VideoSendMessage,
+    AudioSendMessage,
+    LocationSendMessage,
+    StickerSendMessage,
+)
+from linebot.models.sources import (
+    Source,
+    SourceUser,
+    SourceGroup,
+    SourceRoom,
+)
+from linebot.models.template import (
+    TemplateSendMessage,
+    Template,
+    ButtonsTemplate,
+    ConfirmTemplate,
+    CarouselTemplate,
+    CarouselColumn,
+    ImageCarouselTemplate,
+    ImageCarouselColumn,
+>>>>>>> 3b75af30f0813bbb8b993aacbc70f5eb8148add6
 )
 
 app = Flask(__name__)
@@ -43,15 +155,23 @@ def callback():
 
     return 'OK'
 
+<<<<<<< HEAD
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     a = event.message.text
     b =a.lower()
+=======
+@handler.add(MessageEvent, message=TextMessage)
+def usual_message(event):
+    a = event.message.text
+    b = a.lower()
+>>>>>>> 3b75af30f0813bbb8b993aacbc70f5eb8148add6
     if(b=="test"):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=a))
+<<<<<<< HEAD
     elif(b=="assalamualaikum"):
         line_bot_api.reply_message(
             event.reply_token,
@@ -96,6 +216,41 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="lagi masa-masa kelam mengurus Kerja Praktek sama Skripsi nih. Jangan tanya angkatan berapa ya :)"))
+=======
+    elif(b=="info pmb"):
+        line_bot_api.reply_message(
+            event.reply_token, 
+            TemplateSendMessage(
+    alt_text='Confirm template',
+    template=ConfirmTemplate(
+        text='Informasi Penerimaan Mahasiswa Baru',
+        actions=[
+            PostbackAction(
+                label='postback',
+                text='postback text',
+                data='action=buy&itemid=1'
+            ),
+            MessageAction(
+                label='message',
+                text='message text'
+            )
+        ]
+    )
+)
+        )
+    elif(b=="lokasi unsada"):
+        line_bot_api.reply_message(
+            event.reply_token, LocationSendMessage(
+                title='Universitas Darma Persada',
+                address='Jakarta Timur',
+                latitude=35.65910807942215,
+                longitude=139.70372892916203
+            ))
+    elif(b=="siapa kamu"):
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="Yooka"))
+>>>>>>> 3b75af30f0813bbb8b993aacbc70f5eb8148add6
     elif(b=="question"):
         line_bot_api.reply_message(
             event.reply_token,
@@ -103,7 +258,11 @@ def handle_message(event):
     else:
         line_bot_api.reply_message(
             event.reply_token,
+<<<<<<< HEAD
             TextSendMessage(text="Yooka blm bisa jawab chat kamu. Coba chat yang lain, mungkin aku bisa jawab hohoho"))
+=======
+            TextSendMessage(text="wah, sorry nih. Tampaknya soal itu aku belum paham. Coba cek Menu bantuan dibawah."))
+>>>>>>> 3b75af30f0813bbb8b993aacbc70f5eb8148add6
 
 
 if __name__ == "__main__":
