32af4a42ab9f339850b164dade3e3b4e65bbab8b
Kukuh Pradityo <kukuhpradityo@gmail.com>
2018-07-19 20:33:33 +0700

    test input carousel

--- app.py
+++ app.py
@@ -159,6 +159,7 @@ def usual_message(event):
         line_bot_api.reply_message(
             event.reply_token, 
             TemplateSendMessage(
+            alt_text='Carousel template',
             template=CarouselTemplate(
                 columns=[
                     CarouselColumn(
