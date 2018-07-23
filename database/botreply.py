afaba8b238f55fb3933efa14766d7d251b5fde29
Kukuh Pradityo <kukuhpradityo@gmail.com>
2018-07-24 04:16:16 +0700

    test new file

--- database/botreply.py
+++ database/botreply.py
@@ -1,57 +1,11 @@
 from __future__ import unicode_literals
 
-# Keywords Dictionary
-EXPRESSION = {
-    'thankyou': ['terima kasih', 'thx', ''],
-    'greet': ['hello', 'hi', 'hey'],
-    'goodbye': ['bye', 'farewell', 'dadah']
-}
-
-DEFAULT_SWAPWORDS = {
-        'lo':'kamu', 
-        'loe':'kamu', 
-        'lu':'kamu', 
-        'ente':'kamu', 
-        'elu':'kamu',
-        'you':'kamu',
-        'yu':'kamu', 
-        'situ':'kamu', 
-        'u':'kamu', 
-        'i':'aku', 
-        'gue':'aku', 
-        'gw':'aku', 
-        'gwe':'aku', 
-        'gua':'aku',
-        'wa':'aku',
-        'gwa':'aku',
-        'ue':'aku',
-        'gout':'aku',
-    }
-
-DEFAULT_EXCUSES = [
-        'wah, aku ngga ngerti',
-        'suka suka u',
-        'ampun, saya belum ngerti soal ini. ngomong yang lain aja ya.',
-        'wah, sebentar ya. kebelet pipis.',
-        'ah masa',
-        'hahahahaha',
-        'ora paham maksudmu',
-        'gak ngerti. tolong pencerahannya',
-        'mantap gan',
-        'aduh ngga ngerti, coba pakai bahasa yang lebih mudah dimengerti',
-        'jangan disingkat-singkat dong, bingung bacanya',
-        'maaf, ini chatnya pakai Bahasa Indonesia apa bahasa Rusia :p',
-        'asli ngga ngerti',
-        'oh gitu ya, tapi sebenernya aku ngga ngerti',
-        'wkwkwk',
-        'aduh aku pusing baca chat kamu',
-    ]
-
 import os
 import psycopg2
 import json
 import spacy
 import rasa_nlu
+
 from future.utils import with_metaclass
 from abc import ABCMeta
 from linebot.models import Base
@@ -311,4 +265,50 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="Aku ngga ngerti maksud kamu. Coba cek Quick Menu dibawah, mungkin bisa membantu."))
-    
+
+# Keywords Dictionary
+EXPRESSION = {
+    'thankyou': ['terima kasih', 'thx', ''],
+    'greet': ['hello', 'hi', 'hey'],
+    'goodbye': ['bye', 'farewell', 'dadah']
+}
+
+DEFAULT_SWAPWORDS = {
+        'lo':'kamu', 
+        'loe':'kamu', 
+        'lu':'kamu', 
+        'ente':'kamu', 
+        'elu':'kamu',
+        'you':'kamu',
+        'yu':'kamu', 
+        'situ':'kamu', 
+        'u':'kamu', 
+        'i':'aku', 
+        'gue':'aku', 
+        'gw':'aku', 
+        'gwe':'aku', 
+        'gua':'aku',
+        'wa':'aku',
+        'gwa':'aku',
+        'ue':'aku',
+        'gout':'aku',
+    }
+
+DEFAULT_EXCUSES = [
+        'wah, aku ngga ngerti',
+        'suka suka u',
+        'ampun, saya belum ngerti soal ini. ngomong yang lain aja ya.',
+        'wah, sebentar ya. kebelet pipis.',
+        'ah masa',
+        'hahahahaha',
+        'ora paham maksudmu',
+        'gak ngerti. tolong pencerahannya',
+        'mantap gan',
+        'aduh ngga ngerti, coba pakai bahasa yang lebih mudah dimengerti',
+        'jangan disingkat-singkat dong, bingung bacanya',
+        'maaf, ini chatnya pakai Bahasa Indonesia apa bahasa Rusia :p',
+        'asli ngga ngerti',
+        'oh gitu ya, tapi sebenernya aku ngga ngerti',
+        'wkwkwk',
+        'aduh aku pusing baca chat kamu',
+    ]
