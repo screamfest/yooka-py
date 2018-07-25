714fe92f9ce841953b427a0bdcb96dd70e350cb0
Kukuh Pradityo <kukuhpradityo@gmail.com>
2018-07-25 14:26:16 +0700

    test adding event reply

--- app.py
+++ app.py
@@ -194,16 +194,16 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(
-                text="Aku Yooka", 
-                text="Mahasiswa Teknik Informatika di Universitas Darma Persada"))
+                text="Aku Yooka. Mahasiswa Teknik Informatika di Universitas Darma Persada"))
         line_bot_api.reply_message(    
+            event.reply_token,
             TextSendMessage(text="Untuk menambahkan teman bisa search id aku nih @oiw3246b, atau scan QR Code dibawah ini"),
             ImageSendMessage(
                 original_content_url='https://example.com/original.jpg',
                 preview_image_url='https://example.com/preview.jpg'
             ),
             TextSendMessage(text="thank you!")
-                )
+        )
     ################ tier 2 - Get me more!
     elif(b=="more info about unsada"):
         line_bot_api.reply_message(
